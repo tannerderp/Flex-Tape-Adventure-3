60c04140906ac9d6b9050e26cadb636c0b2079dd
Tannerderp <47506918+tannerderp@users.noreply.github.com>
2020-04-22 10:00:51 -0600

    only objects near the player are active
    so less lag my man

---
+++ Flex Tape Adventure 3/Assets/Scripts/World/ObjectLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//making only the objects close to the player enabled so there's no no lag
+public class ObjectLoader : MonoBehaviour
+{
+    private Camera Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = FindObjectOfType<Camera>();
+        Transform[] children = new Transform[transform.childCount];;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            if(Vector2.Distance(child.GetComponent<BoxCollider2D>().ClosestPoint(Camera.transform.position), Camera.transform.position) > Camera.GetComponent<Camera>().orthographicSize * 2) //if the game object is far from the camera, disable it so less lag
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+}
